b46fd9729586f4e6e88156a90960cf8f5b6b199f
Lovkush Agarwal <lovkush@gmail.com>
2021-06-27 11:43:39 +0100

    delete mnemosyne.sql

--- mnemosyne.sql
+++
@@ -1,9 +0,0 @@
-UPDATE cards
-    SET answer = substr(answer, 4, instr(answer, '?') - 4),
-    SET question = substr(question, instr(question, '<img'))
-    WHERE tags LIKE '%africa%' and instr(answer, '?') > 0;
-
-UPDATE cards
-    SET question = substr(question, 4, instr(question, '?') - 4),
-    SET answer = substr(answer, instr(answer, '<img'))
-    WHERE tags LIKE '%africa%' and instr(question, '?') > 0;
